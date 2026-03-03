acf5b79f484c9f9ec366d13a8b5d8f3f7900a342
Jonathan Belden <belden@oakleasimpsonsecurity.com>
2026-03-03 15:14:49 -0600

    initial project scaffolding

---
+++ src/semantic_veritas_tool/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from semantic-veritas-tool!")
---
+++ src/semantic_veritas_tool/semantic_veritas.py
@@ -0,0 +1,5 @@
+# File: src/semantic_veritas_tool/semantic_veritas.py
+# Author: Jonathan Belden
+# Description: A universal tool for setting and determining a project's semantic versioning.
+
+
